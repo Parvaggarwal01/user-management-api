e0f602783b10f3ab0acb40d113494dd7fa91257a
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-02-05 10:37:49 +0530

    Fix Azure deployment logs

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-import express from 'express';
-import userRouter from './routes/users.routes.js'
+import express from "express";
+import userRouter from "./routes/users.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("User Management API is Running")
-})
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
-app.use("/api/users", userRouter)
+app.get("/", (req, res) => {
+  res.send("User Management API is Running");
+});
+
+app.use("/api/users", userRouter);
 
 export default app;
